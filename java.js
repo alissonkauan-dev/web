24dd750dbac4b016ea5d292dbf3f6d96d7e6c9a0
Alisson <alissonkauan18@icloud.com>
2025-09-10 09:26:49 -0300

    atividade-1

---
+++ java.js
@@ -0,0 +1,19 @@
+
+let A = 0, B = 0, C = 0;
+
+A = parseFloat(prompt("INSIRA O PRIMEIRO VALOR: "));
+B = parseFloat(prompt("INSIRA O SEGUNDO VALOR: "));
+
+// condicionais
+
+if (A == B) {
+       C = A + B ;
+        console.log(C); }
+
+
+        else  {
+         C = A * B;
+         console.log(C);
+
+};
+
